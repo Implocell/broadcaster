54361beb75cc50a97f03141e1a53455538d9b24e
Implocell <thomaskgran@gmail.com>
2025-09-17 21:24:47 +0200

    simple hello health

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/health", get(health_handler));
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_handler() -> impl IntoResponse {
+    StatusCode::OK
 }
